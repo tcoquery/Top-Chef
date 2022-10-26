2ea039f72098d783a01ed95654dca38a7a75906f
tcoquery <tcoquery@gmail.com>
2022-10-26 11:57:05 +0200

    add candidates to a team

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,8 +2,13 @@ class CandidatesController < ApplicationController
 
   def index
     @candidates = Candidate.all
+  end
 
-    @leagues_teams = Team.where('league_id = ?', params[:league])
+  def create
+    @user_team = Team.where('league_id = ? AND user_id = ?', params[:league], current_user.id).first
+
+    @user_team.candidate_one_id = params[:candidate_id]
+    @user_team.save
   end
 
 end
