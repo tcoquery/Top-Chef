9119fee0de534037bbf4e23d96fd5d5ff3335c41
tcoquery <tcoquery@gmail.com>
2022-10-26 14:54:11 +0200

    fill the appropriate candidate slot

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -7,8 +7,19 @@ class CandidatesController < ApplicationController
   def create
     @user_team = Team.where('league_id = ? AND user_id = ?', params[:league], current_user.id).first
 
-    @user_team.candidate_one_id = params[:candidate_id]
+    if @user_team.candidate_one_id.nil?
+      @user_team.candidate_one_id = params[:candidate_id]
+    elsif @user_team.candidate_two_id.nil?
+      @user_team.candidate_two_id = params[:candidate_id] 
+    elsif @user_team.candidate_three_id.nil?
+      @user_team.candidate_three_id = params[:candidate_id] 
+    elsif @user_team.candidate_four_id.nil?
+      @user_team.candidate_four_id = params[:candidate_id]    
+    end
+
     @user_team.save
+
+    redirect_to candidates_path
   end
 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,8 @@ class Team < ApplicationRecord
   
   belongs_to :user
   belongs_to :league
-  belongs_to :candidate_one, class_name: "Candidate"
-  belongs_to :candidate_two, class_name: "Candidate"
-  belongs_to :candidate_three, class_name: "Candidate"
-  belongs_to :candidate_four, class_name: "Candidate"
+  belongs_to :candidate_one, class_name: "Candidate", optional: true
+  belongs_to :candidate_two, class_name: "Candidate", optional: true
+  belongs_to :candidate_three, class_name: "Candidate", optional: true
+  belongs_to :candidate_four, class_name: "Candidate", optional: true
 end
