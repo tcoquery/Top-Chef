0630d774a2bffa2c2f31443c74d1aa23558d9594
tcoquery <tcoquery@gmail.com>
2022-11-03 11:13:41 +0100

    change the drafting team upon drafting a candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,12 +6,10 @@ class CandidatesController < ApplicationController
     @league_teams = Team.where('league_id = ?', params[:league])
 
     @league_teams.each do |team| 
-      unless team.candidate_one.nil?
-        @drafted_candidates.push(team.candidate_one.id)
-        @drafted_candidates.push(team.candidate_two.id)
-        @drafted_candidates.push(team.candidate_three.id)
-        @drafted_candidates.push(team.candidate_four.id)
-      end
+      @drafted_candidates.push(team.candidate_one.id) unless team.candidate_one.nil? 
+      @drafted_candidates.push(team.candidate_two.id) unless team.candidate_two.nil? 
+      @drafted_candidates.push(team.candidate_three.id) unless team.candidate_three.nil? 
+      @drafted_candidates.push(team.candidate_four.id) unless team.candidate_four.nil? 
     end
 
     @candidates = Candidate.all
@@ -19,6 +17,7 @@ class CandidatesController < ApplicationController
 
   def create
     @user_team = Team.where('league_id = ? AND user_id = ?', params[:league], current_user.id).first
+    @league_teams = Team.where('league_id = ?', params[:league]).length
 
     if @user_team.candidate_one_id.nil?
       @user_team.candidate_one_id = params[:candidate_id]
@@ -30,9 +29,17 @@ class CandidatesController < ApplicationController
       @user_team.candidate_four_id = params[:candidate_id]    
     end
 
+    if @league_teams == @user_team.draft_order 
+      Team.where('league_id = ? AND draft_order = ?', params[:league], 1).first.update(can_draft: true)
+    else 
+      Team.where('league_id = ? AND draft_order = ?', params[:league], @user_team.draft_order + 1).first.update(can_draft: true)
+    end 
+
+    @user_team.can_draft = false
+
     @user_team.save
 
-    redirect_to candidates_path
+    redirect_to league_path(id: params[:league])
   end
 
 end
